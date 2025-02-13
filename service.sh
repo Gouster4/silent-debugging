904f56d40d06dc2ef5c5e95eb11d73dcb45e40b9
Pavel Horniak <35545900+Gouster4@users.noreply.github.com>
2025-02-13 09:41:57 +0100

    Update service.sh
    Adjust some sleep trickery

--- service.sh
+++ service.sh
@@ -4,6 +4,14 @@ while (($i > 60)) && [ $(getprop sys.boot_completed) != true ]; do
     sleep 10
     ((retry++))
 done
+/system/bin/sh $MODDIR/action.sh
 sleep 10
 /system/bin/sh $MODDIR/action.sh
-
+sleep 10
+/system/bin/sh $MODDIR/action.sh
+sleep 10
+/system/bin/sh $MODDIR/action.sh
+sleep 10
+/system/bin/sh $MODDIR/action.sh
+sleep 10
+/system/bin/sh $MODDIR/action.sh
